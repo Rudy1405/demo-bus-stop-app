f4b829afa3bf5b7640eadd60c40eeefeab94169e
OS <os@OSs-MacBook-Pro.local>
2019-11-07 03:00:22 -0600

    www

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+}); 
  
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
